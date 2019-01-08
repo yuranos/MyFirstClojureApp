a91779923b170fae620786401b9e64ac85934f7c
Yura Nosenko <yura@attendify.com>
2019-01-08 10:42:19 +0200

    Finishing reify

--- src/clojure/com/yuranos/general/datatypes.clj
+++ src/clojure/com/yuranos/general/datatypes.clj
@@ -1,6 +1,6 @@
 (ns com.yuranos.general.datatypes
-  (:import (clojure.lang ArityException)
-           (clojure.java.api Clojure)))
+  (:import (clojure.lang ISeq)
+           (com.yuranos.javafiles InterfaceWithDefaultMethod ClassWithMission)))
 
 ;Gen-class came first, followed by deftype in clojure 1.2.
 ; Deftype is preferred, and has better performance characteristics,
@@ -18,16 +18,16 @@
 
 (defn myproxy[& i]
   (proxy [;class must be first
-          com.yuranos.javafiles.ClassWithMission
-          clojure.lang.ISeq
-          com.yuranos.javafiles.InterfaceWithDefaultMethod] ["I'm useless"]
+          ClassWithMission
+          ISeq
+          InterfaceWithDefaultMethod] ["I'm useless"]
 
     ;For ISeq
     (seq [] (sort i))
     (toString [] (apply str (interpose "-" i)))
 
-    ;For com.yuranos.javafiles.InterfaceWithDefaultMethod
-    (printStatic ([] (com.yuranos.javafiles.InterfaceWithDefaultMethod/myStaticMethod)))
+    ;For InterfaceWithDefaultMethod
+    (printStatic ([] (InterfaceWithDefaultMethod/myStaticMethod)))
     ;Seems like default methods are not supported: UnsupportedOperationException myDefaultMethod
     (printSomethingOrDefault ([] (.myDefaultMethod this))
                 ([str] (prn "printSomethingOrDefault called with arg:" str)))
@@ -37,7 +37,7 @@
     (printParam [param] (prn param))
     (myDefaultMethod [] (prn "Can't call default, but can override it. Welcome to Clojure!"))
 
-    ;For com.yuranos.javafiles.ClassWithMission
+    ;For ClassWithMission
     ;You can override your mission no problem.
     (getClassMission [] (prn "Mission has been overridded. Be careful."))
     (abstractMethod ([] (prn (.getClassMission this)))
@@ -48,4 +48,31 @@
 ;(.printSomethingOrDefault (myproxy 4 3 2 1) "Param")
 ;unless we are dealing wih purely Clojure contracts (like seq)
 
-;TODO: Reify
+;For reify multi-arity doesn't work. You need to declare all overloads separately.
+;(meta ^{:k :v} (reify com.yuranos.javafiles.InterfaceWithDefaultMethod (toString [this] "foo")))
+(.printSomethingOrDefault
+  (let [i [1 4 3 2]]
+    (reify
+      ISeq
+      (seq [_] (sort i))
+      (toString [_] (apply str (interpose "-" i)))
+
+      InterfaceWithDefaultMethod
+      (printStatic [_] (InterfaceWithDefaultMethod/myStaticMethod))
+      ;Seems like default methods are not supported: UnsupportedOperationException myDefaultMethod
+      (printSomethingOrDefault [this] (.myDefaultMethod this))
+      (printSomethingOrDefault [_ str] (prn "printSomethingOrDefault called with arg:" str))
+      ;(printSomethingOrDefault ([this] (.myDefaultMethod this))
+      ;  ([str] (prn "printSomethingOrDefault called with arg:" str)))
+      ;Without this: UnsupportedOperationException printParam
+      ;com.yuranos.general.datatypes.proxy$com.yuranos.javafiles.ClassWithMission$ISeq$InterfaceWithDefaultMethod$9bd1e475.printParam (:-1)
+      ;when called
+      (printParam [_ param] (prn param))
+      (myDefaultMethod [_] (prn "Can't call default, but can override it. Welcome to Clojure!"))
+      ;CompilerException java.lang.IllegalArgumentException:
+      ; only interfaces are supported, had: com.yuranos.javafiles.ClassWithMission
+      ;ClassWithMission
+      ))
+  "Something arg"
+
+  )
